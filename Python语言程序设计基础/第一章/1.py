d2afe0de519d8e02b62a22ad9d449223739f0e26
Apologise <603061018@qq.com>
2017-05-27 22:38:12 +0800

    第一章以及第二章

---
+++ Python语言程序设计基础/第一章/1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 27 10:33:53 2017
+
+@author: Administrator
+"""
+
+str1 = input('请输入一个人的名字:')
+str2 = input('请输入一个国家的名字:')
+print("世界那么大，{1}想去{0}看看。".format(str1,str2))
---
+++ Python语言程序设计基础/第一章/8.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 27 16:52:42 2017
+
+@author: Administrator
+"""
+
+from turtle import *
+color('red', 'yellow')
+begin_fill()
+while True:
+    forward(200)
+    left(170)
+    """
+    if(abs(pos()) < 1):
+        break
+    """
+end_fill()
+don()
