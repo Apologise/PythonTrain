5d64ac7f485e6c18f12d1a04025afb0391da518d
Apologise <603061018@qq.com>
2017-08-05 20:12:45 +0800

    .,.

--- 序列判断.py
+++ 序列判断.py
@@ -1,5 +1,13 @@
-L = 'abcd'
+
 
 def func(L):
 	up_str = sorted(L)
 	down_str = sorted(L, reverse=True)
+	if L == up_str：
+		print('UP')
+	elif L == down_str:
+		print('DOWN')
+	else:
+		print('WRONG')
+
+func(L)
